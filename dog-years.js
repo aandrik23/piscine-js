25fa960faffeec1ebdb60bc1e6165fd8b70c5aee
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-08 01:02:55 +0300

    dog years

---
+++ dog-years.js
@@ -0,0 +1,22 @@
+function dogYears(planet,seconds) {
+    const orbitalPeriods = {
+        earth :1.0,
+        mercury: 0.2408467,
+        venus: 0.61519726,
+        mars: 1.8808158,
+        jupitter: 11.862615,
+        saturn: 29.447498,
+        uranus: 84.016846,
+        nepture: 164.79132
+    };
+
+    const Earth_Year_Seconds = 31557600; // seconds in one Earth year
+    const earthYears = seconds / Earth_Year_Seconds;
+    const planetYears = earthYears / orbitalPeriods[planet.toLowerCase()];
+    const dogYears = planetYears * 7;
+
+    return Number(dogYears.toFixed(2));
+    }
+
+console.log(dogYears("earth", 1000000000)); 
+console.log(dogYears("mars", 1000000000));
