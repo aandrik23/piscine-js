61ac5d792aa325e53aca8f937e63922e2407fe72
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-07 20:36:05 +0300

    change nested

--- declarations.js
+++ declarations.js
@@ -1,16 +1,18 @@
 const escapeStr = "\"`/\\'"
 const arr = Object.freeze([4, '2'])
+const nested = Object.freeze({
+    arr: Object.freeze([4, undefined, '2']),
+    obj: Object.freeze({
+        str: "my name is Andrew.",
+        num: 24,
+        bool: true,
+    })
+});
 const obj = Object.freeze({
     str: "Hello, World!",
     num: 16,
     bool: true,
     undef: undefined,
-    nested: Object.freeze({
-        arr: Object.freeze([4, undefined, '2']),
-        obj: Object.freeze({
-            str: "my name is Andrew.",
-            num: 24,
-            bool: true,
-        })
-    })
+    nested: nested
+        
 });
