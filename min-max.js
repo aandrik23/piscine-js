8852a4f91c9b44392a16aeb294f56805666ebd2f
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-08 23:10:03 +0300

    min-max

---
+++ min-max.js
@@ -0,0 +1,14 @@
+function max(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
+
+function min(num1, num2) {
+    return num1 < num2 ? num1 : num2;
+}
+
+console.log(max(5,10));
+console.log(max(10,5));
+console.log(max(5,5));
+console.log(min(5,10));
+console.log(min(10,5));
+console.log(min(5,5));
