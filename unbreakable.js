cff5caa97d495e54d2af90fbc21de415c783cfee
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-11 02:09:05 +0300

    unbreakable

--- unbreakable.js
+++ unbreakable.js
@@ -1,36 +1,32 @@
-function join(arr, sep) {
-    if (sep === null) {
-        sep = ",";
+const split = (str, separator) => {
+    const result = [];
+    let temp = '';
+    
+    for (let i = 0; i < str.length; i++) {
+  
+      if (separator && str.slice(i, i + separator.length) === separator) {
+        result.push(temp);
+        temp = '';
+        i += separator.length - 1;
+      } else {
+        temp += str[i];
+      }
     }
-    var result = arr[0].toString();
-    for (var i = 1; i < arr.length; i++) {
-        result += sep + arr[i];
-    }
+  
+    result.push(temp);
     return result;
-}
+};
 
-function split(str, sep) {
-    // Split a given string using a multi-character separator
-    // and return an array of the results.
-    if (sep === null) {
-        sep = ",";
-    }
-    var result = [];
-    if (sep === "") {
-        for (var i = 0; i < str.length; i++) {
-            result.push(str[i]);
+const join = (arr, separator) => {
+    let result = '';
+    
+    for (let i = 0; i < arr.length; i++) {
+        result += arr[i];
+        if (i < arr.length - 1 && separator) {
+            result += separator;
         }
-        return result;
     }
-    var end = str.indexOf(sep);
-    while (end > -1) {
-        end = str.indexOf(sep);
-        if (end === -1) {
-            break;
-        }
-        result.push(str.slice(0, end));
-        str = str.slice(end + sep.length);
-    }
-    result.push(str);
+    
     return result;
 }
+  
