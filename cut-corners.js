8d5197815a77dbe160d71b441dfe39f632efdee8
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-11 01:56:55 +0300

    cut-corners

--- cut-corners.js
+++ cut-corners.js
@@ -1,43 +1,22 @@
 const trunc = n => {
-    let i = 0;
-    if (n >= 0) {
-      while (i + 1 <= n) i++;
-    } else {
-      while (i - 1 >= n) i--;
-    }
-    return i;
+    return n < 0 ? n - (n - (n / 1)) : n - (n - (n / 1));
   };
   
   const floor = n => {
-    let i = 0;
-    if (n >= 0) {
-      while (i + 1 <= n) i++;
-    } else {
-      while (i - 1 > n) i--;
-      if (i > n) i--;
-    }
-    return i;
+    const i = n - (n - (n / 1));
+    return (n < 0 && n !== i) ? i - 1 : i;
   };
-  
   
   const ceil = n => {
-    let i = 0;
-    if (n >= 0) {
-      while (i < n) i++;
-    } else {
-      while (i - 1 >= n) i--;
-    }
-    return i;
+    const i = n - (n - (n / 1));
+    return (n > 0 && n !== i) ? i + 1 : i;
   };
   
-  
   const round = n => {
-    let i = trunc(n);
-    const frac = n - i;
-    if (n >= 0) {
-      return frac >= 0.5 ? i + 1 : i;
-    } else {
-      return frac <= -0.5 ? i - 1 : i;
-    }
+    const i = n - (n - (n / 1));
+    const f = n - i;
+    return n >= 0
+      ? (f >= 0.5 ? i + 1 : i)
+      : (f <= -0.5 ? i - 1 : i);
   };
   
