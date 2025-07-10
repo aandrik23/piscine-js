9b28f3862412628c465c0cf22024bf3811beab42
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-10 20:43:50 +0300

    keep-cut

---
+++ keep-cut.js
@@ -0,0 +1,16 @@
+const cutFirst = str => str.slice(2);
+const cutLast = str => str.slice(0, -2);
+const cutFirstLast = str => str.slice(2, -2);
+const keepFirst = str => str.slice(0, 2);
+const keepLast = str => str.slice(-2);
+const keepFirstLast = str => str.slice(0, 2) + str.slice(-2);
+
+
+const s = "hello world";
+
+console.log(cutFirst(s));       
+console.log(cutLast(s));        
+console.log(cutFirstLast(s));   
+console.log(keepFirst(s));     
+console.log(keepLast(s));       
+console.log(keepFirstLast(s));  
