a5783084b6537ce82a18f785fd21c61f79b66562
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-11 02:10:06 +0300

    unbreakable

--- unbreakable.js
+++ unbreakable.js
@@ -1,10 +1,17 @@
 const split = (str, separator) => {
+    if (separator === '') {
+      const result = [];
+      for (let i = 0; i < str.length; i++) {
+        result.push(str[i]);
+      }
+      return result;
+    }
+  
     const result = [];
     let temp = '';
-    
-    for (let i = 0; i < str.length; i++) {
   
-      if (separator && str.slice(i, i + separator.length) === separator) {
+    for (let i = 0; i < str.length; i++) {
+      if (str.slice(i, i + separator.length) === separator) {
         result.push(temp);
         temp = '';
         i += separator.length - 1;
@@ -15,18 +22,13 @@ const split = (str, separator) => {
   
     result.push(temp);
     return result;
-};
-
-const join = (arr, separator) => {
+  };
+  const join = (arr, separator) => {
     let result = '';
-    
     for (let i = 0; i < arr.length; i++) {
-        result += arr[i];
-        if (i < arr.length - 1 && separator) {
-            result += separator;
-        }
+      result += arr[i];
+      if (i < arr.length - 1) result += separator;
     }
-    
     return result;
-}
-  
+  };
+    
