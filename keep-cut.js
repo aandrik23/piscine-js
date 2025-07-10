403da37fd391491b1ab51227f27ce646448b3150
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-10 20:51:40 +0300

    keep-cut

--- keep-cut.js
+++ keep-cut.js
@@ -4,7 +4,7 @@ const cutFirstLast = str => str.slice(2, -2);
 const keepFirst = str => str.slice(0, 2);
 const keepLast = str => str.slice(-2);
 const keepFirstLast = str => {
-    if (str.length <= 2) return str;
+    if (str.length < 4) return str;
     return str.slice(0, 2) + str.slice(-2);
   };
 
