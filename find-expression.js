7f2e863d318c2739f2b52fe67d33cc1518e65e79
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-11 22:31:17 +0300

    find

--- find-expression.js
+++ find-expression.js
@@ -1,10 +1,11 @@
 const findExpression = (target) => {
     const ops = [
-      { symbol: add4, apply: x => x + (add4.length - 2) }, 
-      { symbol: mul2, apply: x => x * (mul2.length - 1) }, 
+      { symbol: add4, apply: x => x + (add4.length - 2) },
+      { symbol: mul2, apply: x => x * (mul2.length - 1) },
     ];
   
     const queue = [{ value: 1, path: '1' }];
+    const seen = new Set([1]);
   
     while (queue.length) {
       const { value, path } = queue.shift();
@@ -12,12 +13,17 @@ const findExpression = (target) => {
       if (value > target) continue;
   
       for (const { symbol, apply } of ops) {
-        queue.push({ value: apply(value), path: path + ' ' + symbol });
+        const next = apply(value);
+        if (!seen.has(next)) {
+          seen.add(next);
+          queue.push({ value: next, path: path + ' ' + symbol });
+        }
       }
     }
   
     return undefined;
   };
+  
   
 
 console.log(findExpression(8));   
