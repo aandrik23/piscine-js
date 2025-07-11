dd4f52fc1d09ded84d1c2783289a8b3eb53f006b
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-11 21:38:43 +0300

    get

--- get.js
+++ get.js
@@ -10,6 +10,6 @@ const get = (src, path) => {
     return current;
   };
 
-  console.log(get({ a: { b: { c: 42 } } }, 'a.b.c')); // 42
-    console.log(get({ a: { b: { c: 42 } } }, 'a.b.d')); // undefined
-    console.log(get({},    'a.b.c.d')); // undefined
+  console.log(get({ a: { b: { c: 42 } } }, 'a.b.c')); 
+    console.log(get({ a: { b: { c: 42 } } }, 'a.b.d')); 
+    console.log(get({},    'a.b.c.d')); 
