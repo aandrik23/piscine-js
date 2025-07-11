9c028459af5af48f3a8de6f1be2646eec4e77564
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-11 21:56:04 +0300

    pyramid

--- pyramid.js
+++ pyramid.js
@@ -1,18 +1,21 @@
 const pyramid = (char, height) => {
+    char = String(char);
     height = Number(height);
   
     if (!Number.isInteger(height) || height <= 0) return '';
   
-    let result = '';
+    const result = [];
   
     for (let i = 1; i <= height; i++) {
-      const spaces = ' '.repeat(height - i);
       const chars = char.repeat(2 * i - 1);
-      result += spaces + chars;
-      if (i < height) result += '\n';
+      const totalWidth = char.length * (2 * height - 1);
+      const lineWidth = char.length * (2 * i - 1);
+      const padding = ' '.repeat((totalWidth - lineWidth) / 2);
+      result.push(padding + chars);
     }
   
-    return result;
+    return result.join('\n');
   };
 
 console.log(pyramid('*', 5));
+console.log(pyramid('{}', 3));
