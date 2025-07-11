3187f726df1db11ea83b9dd4ed8d4a6d372abf32
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-11 22:20:26 +0300

    find

---
+++ find-expression.js
@@ -0,0 +1,21 @@
+const add4 = '+4';
+const mul2 = '*2';
+
+const findExpression = (target) => {
+  const queue = [{ value: 1, path: '1' }];
+
+  while (queue.length) {
+    const { value, path } = queue.shift();
+    if (value === target) return path;
+    if (value > target) continue;
+
+    queue.push({ value: value + 4, path: path + ' ' + add4 });
+    queue.push({ value: value * 2, path: path + ' ' + mul2 });
+  }
+
+  return undefined;
+};
+
+console.log(findExpression(8));   
+console.log(findExpression(15));  
+console.log(findExpression(16));   
