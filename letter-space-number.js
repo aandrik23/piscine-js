bf499a4411b9c5d97b52a27bf8b29ce2d2be3ab5
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-12 18:35:31 +0300

    letterspacenumber

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,6 +1,7 @@
 function letterSpaceNumber(str) {
-    const regex = /\b([a-zA-Z]) (\d)(?![a-zA-Z])/g;
-    return [...str.matchAll(regex)].map(match => match[0]);
-}
+    const regex = /([a-zA-Z]) (\d)(?!\d|[a-zA-Z])/g;
+    return [...str.matchAll(regex)].map(m => m[0]);
+  }
 
 console.log(letterSpaceNumber('a 1 b 2c 3 d 4x e 5'));
+console.log(letterSpaceNumber('He is 8 or 9 years old, not 10.'));
