75bcda41545d153e2f98de7020e7583d5ebbf2e5
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-22 21:44:26 +0300

    manipulate values

--- manipulate-values.js
+++ manipulate-values.js
@@ -1,41 +1,5 @@
-function filterValues(obj, callback) {
-    const result = {};
-    for (let key in obj) {
-      if (Object.prototype.hasOwnProperty.call(obj, key)) {
-        const value = obj[key];
-        if (callback(value)) {
-          result[key] = value;
-        }
-      }
-    }
-    return result;
-  }
-  
-  function mapValues(obj, callback) {
-    const result = {};
-    for (let key in obj) {
-      if (Object.prototype.hasOwnProperty.call(obj, key)) {
-        result[key] = callback(obj[key]);
-      }
-    }
-    return result;
-  }
-  
-  function reduceValues(obj, callback, initialValue) {
-    let accumulator = initialValue;
-  
-    for (let key in obj) {
-      if (Object.prototype.hasOwnProperty.call(obj, key)) {
-        let value = obj[key];
-  
-        if (typeof value === "object" && value !== null && !Array.isArray(value)) {
-          value = reduceValues(value, callback, initialValue);
-        }
-  
-        accumulator = callback(accumulator, value);
-      }
-    }
-  
-    return accumulator;
-  }
-  
+const filterValues = (obj, cond) => Object.keys(obj).reduce((result, key) => (cond(obj[key]) && (result[key] = obj[key]), result), {});
+
+const mapValues = (obj, cond) => Object.keys(obj).reduce((result, key) => ((result[key] = cond(obj[key])), result), {});
+
+const reduceValues = (obj, cond, start = 0) => Object.keys(obj).reduce((val, key) => cond(val, obj[key]), start);
