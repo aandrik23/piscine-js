a02ac6c0fdc91f46137c3fd10451ab8d107e39f4
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-23 20:57:54 +0300

    deep copy

--- deep-copy.js
+++ deep-copy.js
@@ -1,8 +1,8 @@
-function deepCopy(value) {
+function deepCopy(obj) {
     if (Array.isArray(obj)) {
         return obj.map(deepCopy);
     } else if (Object.prototype.toString.call(obj).slice(8, -1) === 'Object') {
-        return Object.fromEntries(Object.entries(obj).map(([key, val]) => [key, deepCopy(val)]));
+        return Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, deepCopy(value)]));
     } else {
         return obj;
     }
