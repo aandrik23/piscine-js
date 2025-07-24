4bcbe5bc336a185937848deea28eca555c1f5382
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-24 20:39:52 +0300

    debounce

--- debounce.js
+++ debounce.js
@@ -10,3 +10,26 @@ function debounce(fn, delay) {
     }, delay);
 };
 }
+
+function opDebounce(fn, delay, leading = false) {
+    let timer = null;
+    let called = false;
+  
+    return function (...args) {
+      if (timer) {
+        clearTimeout(timer);
+      }
+  
+      if (leading && !called) {
+        fn.apply(this, args);
+        called = true;
+      }
+  
+      timer = setTimeout(() => {
+        if (!leading) {
+          fn.apply(this, args); 
+        }
+        called = false; 
+      }, delay);
+    };
+  }
