401c1637c419b1e7bec42f6addf10c4ec2eda27b
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-25 18:09:35 +0300

    keep trying or give up

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -15,14 +15,14 @@ function retry(count = 3, callback = async () => {}) {
   
   function timeout(delay = 0, callback = async () => {}) {
     return async function (...args) {
-      const timeout = new Promise((resolve) =>
-        setTimeout(resolve, delay, Error("timeout")),
+      const timeoutPromise = new Promise((_, reject) =>
+        setTimeout(() => reject(new Error("timeout")), delay)
       );
-      const functionCall = new Promise((resolve) => resolve(callback(...args)));
-      const res = await Promise.race([timeout, functionCall]).then((res) => res);
-      if (res instanceof Error) {
-        throw res;
-      }
-      return res;
+  
+      return Promise.race([
+        callback(...args),   
+        timeoutPromise        
+      ]);
     };
   }
+  
