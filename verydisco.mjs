3a1e9e0cab566bd7494986bdc69f7f8b4152241a
aandriko <andreasrafailandrikopoulos@gmail.com>
2025-07-28 21:24:09 +0300

    very disco

---
+++ verydisco.mjs
@@ -0,0 +1,20 @@
+const input = process.argv[2];
+
+if (!input) {
+  console.log("Please provide a word or sentence!");
+  process.exit(1);
+}
+
+
+const words = input.split(" ");
+
+
+const discoWords = words.map(word => {
+  const mid = Math.ceil(word.length / 2);
+  const firstHalf = word.slice(0, mid);
+  const secondHalf = word.slice(mid);
+  return secondHalf + firstHalf;
+});
+
+
+console.log(discoWords.join(" "));
